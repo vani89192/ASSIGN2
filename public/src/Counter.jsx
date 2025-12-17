11b9c3ae02f32b8747714a80bdbef0e8a40f6521
vani89192 <raparthysrivani@gmail.com>
2025-12-17 22:53:28 +0530

    Add files via upload

---
+++ public/src/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+export default Counter;
